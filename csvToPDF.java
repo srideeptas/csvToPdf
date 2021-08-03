57fe73ab138a9602985d93ee60feda30ba92f1c6
Srideepta Sarkar <63389152+srideeptas@users.noreply.github.com>
2021-08-03 15:47:23 +0530

    Update csvToPDF.java

--- csvToPDF.java
+++ csvToPDF.java
@@ -1,62 +1,96 @@
 package com.demo.CovertToPDF;
 
 import java.io.FileOutputStream;
-import java.io.*;
 import au.com.bytecode.opencsv.CSVReader;
 import java.io.FileReader;
-import java.util.*;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.*;
-public class csv2pdf {  
+
+class csv2pdf {  
+	
+	/*Method for adding Main Header*/
+	void addHeader(Document my_pdf_data)throws Exception
+	{
+		
+		PdfPTable table = new PdfPTable(1);
+		table.setTotalWidth(800);
+		Font f = new Font(Font.FontFamily.TIMES_ROMAN, 18, Font.BOLD, BaseColor.WHITE);
+		
+		PdfPCell cell = new PdfPCell(new Phrase("MAIN HEADER",f));
+		cell.setBackgroundColor(new BaseColor(19, 117, 173));
+		cell.setRowspan(2);
+		cell.setBorder(Rectangle.NO_BORDER);
+		cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+		table.addCell(cell);
+		my_pdf_data.add(table);
+	}
+	
+	/*Method for adding subheader*/
+	void addSubHeader(Document my_pdf_data)throws Exception
+	{		
+		my_pdf_data.add( Chunk.NEWLINE );
+
+		Paragraph para1 = new Paragraph("This is the subheader",
+        		FontFactory.getFont(FontFactory.TIMES_ROMAN,12, Font.ITALIC, BaseColor.BLACK));
+		//para1.setAlignment(Element.ALIGN_CENTER);
+		para1.setAlignment(Paragraph.ALIGN_CENTER);
+        my_pdf_data.add(para1);
+        my_pdf_data.add( Chunk.NEWLINE );
+	}
+	
+	/*Method for parsing and adding table rows to pdf*/
+	void addTable(Document my_pdf_data,CSVReader reader)throws Exception {
+		
+		PdfPTable my_first_table = new PdfPTable(4); 		
+		my_first_table.setWidths(new int[]{50, 50, 50, 100}); //no. of width parameters = no. of columns
+		String [] nextLine;
+		int lnNum = 0;
+		//creating table rows
+        PdfPCell table_cell;
+        
+        /* Step -3: Loop through CSV file and convert data to PDF table */
+        while ((nextLine = reader.readNext()) != null) {
+                lnNum++;        
+                table_cell=new PdfPCell(new Phrase(nextLine[0]));
+                my_first_table.addCell(table_cell);
+                table_cell=new PdfPCell(new Phrase(nextLine[1]));
+                my_first_table.addCell(table_cell); 
+                table_cell=new PdfPCell(new Phrase(nextLine[2]));
+                my_first_table.addCell(table_cell);
+                table_cell=new PdfPCell(new Phrase(nextLine[3]));
+                my_first_table.addCell(table_cell);
+                /* number of cells to be added according to the number of columns in input file*/
+        }
+        my_pdf_data.add(my_first_table);  
+       
+		
+	}
+	void addFooter(Document my_pdf_data)throws Exception{
+		my_pdf_data.add( Chunk.NEWLINE );
+        Paragraph para2= new Paragraph("Code by Srideepta Sarkar",
+        		FontFactory.getFont(FontFactory.TIMES_ROMAN,9, Font.BOLD, BaseColor.GREEN));
+        my_pdf_data.add(para2);
+	}
+	
         public static void main(String[] args) throws Exception{
                 
-                /* Step -1 : Read input CSV file in Java */
+                /*Reading input CSV file in Java */
                 String inputCSVFile = "C:\\Users\\hp\\Documents\\csv_to_pdf.csv";
                 CSVReader reader = new CSVReader(new FileReader(inputCSVFile));
-                /* Variables to loop through the CSV File */
                 
-                String [] nextLine; /* for every line in the file */   
-                
-                int lnNum = 0; /* line number */
-                /* Step-2: Initialize PDF documents - logical objects */
+                csv2pdf C = new csv2pdf();
+
+                /*Initializing PDF documents - logical objects */
                 Document my_pdf_data = new Document();
                 PdfWriter.getInstance(my_pdf_data, new FileOutputStream("C:\\\\Users\\\\hp\\\\Documents\\\\converted_PDF_File.pdf"));
-                my_pdf_data.open();            
-                PdfPTable my_first_table = new PdfPTable(4); 
-                // set the parameter as per the number of columns in the input file
                 
-                my_first_table.setWidths(new int[]{50, 50, 50, 100}); //no. of width parameters = no. of columns
+                my_pdf_data.open();  
                 
-                Paragraph para = new Paragraph("This is the Header",
-                		FontFactory.getFont(FontFactory.TIMES_ROMAN,18, Font.BOLD, BaseColor.RED));
-                my_pdf_data.add(para);
-                my_pdf_data.add( Chunk.NEWLINE );
-                my_pdf_data.add( Chunk.NEWLINE );
+                C.addHeader(my_pdf_data);
+                C.addSubHeader(my_pdf_data);
+                C.addTable(my_pdf_data,reader);
+                C.addFooter(my_pdf_data);
                 
-                PdfPCell table_cell;
-                
-                /* Step -3: Loop through CSV file and convert data to PDF table */
-                while ((nextLine = reader.readNext()) != null) {
-                        lnNum++;        
-                        table_cell=new PdfPCell(new Phrase(nextLine[0]));
-                        my_first_table.addCell(table_cell);
-                        table_cell=new PdfPCell(new Phrase(nextLine[1]));
-                        my_first_table.addCell(table_cell); 
-                        table_cell=new PdfPCell(new Phrase(nextLine[2]));
-                        my_first_table.addCell(table_cell);
-                        table_cell=new PdfPCell(new Phrase(nextLine[3]));
-                        my_first_table.addCell(table_cell);
-                        /* number of cells to be added according to the number of columns in input file*/
-                }
-               
-                /* Converting table to pdf file and closing the file */
-                my_pdf_data.add(my_first_table);  
-                
-                //optional part 
-                my_pdf_data.add( Chunk.NEWLINE );
-                Paragraph para2= new Paragraph("Code by Srideepta Sarkar",
-                		FontFactory.getFont(FontFactory.TIMES_ROMAN,9, Font.BOLD, BaseColor.GREEN));
-                my_pdf_data.add(para2);
                 my_pdf_data.close();            
         }
 }
